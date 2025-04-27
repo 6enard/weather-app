f34a29eb4e97a0fb57b81629445cdcfc27daba6f
Bernard <Bernard.koech@student.moringaschool.com>
2025-04-27 23:00:44 +0300

    made sure controllers and routes work with endpoints

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WeatherController;
+
 
 Route::get('weather/{city}/{unit?}', [WeatherController::class, 'getWeather']);
 
